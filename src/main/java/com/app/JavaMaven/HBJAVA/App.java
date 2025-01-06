a6b69a48448c3bb829dd39103e1b3164cb15b85d
angelena <angelena.shiny@gmail.com>
2025-01-06 14:05:28 +0530

    initial commit

---
+++ src/main/java/com/app/JavaMaven/HBJAVA/App.java
@@ -0,0 +1,10 @@
+package com.app.JavaMaven.HBJAVA;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Created a mven java Project named HBJAVA");
+    }
+}
